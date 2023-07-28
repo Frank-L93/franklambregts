08e2971c088ebc3e44e4c5b488b46e10d619f459
Frank-L93 <frank.lambregts@outlook.com>
2023-07-28 11:50:33 +0200

    Chess page add

---
+++ resources/views/pages/chess.blade.php
@@ -0,0 +1,11 @@
+@extends('welcome')
+
+@section('title', 'Chess')
+
+@section('content')
+<main class="max-w-7xl mx-auto p-6 lg:p-8">
+    <div class="text-2xl font-semibold mx-auto text-green-300 text-center">Chess</div> 
+    <livewire:hobby />
+
+</main>
+@endsection
