2602370c3f992f5af85a23e04481fa42063b4ab1
Frank Lambregts <frank@franklambregts.com>
2024-05-03 09:53:49 +0200

    Update resume.blade.php

--- resources/views/pages/resume.blade.php
+++ resources/views/pages/resume.blade.php
@@ -7,6 +7,28 @@
 <div class="text-2xl font-semibold mx-auto text-yellow-300 text-center">Resume</div>
 
 <ol >
+    <li class="border-l-2 odd:border-green-600 even:border-purple-600">
+        <div class="md:flex flex-start" id="2024">
+          <div class="bg-purple-600 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" class="text-white w-3 h-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path fill="currentColor" d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"></path>
+            </svg>
+          </div>
+          <div class="block p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-6 mb-10">
+            <div class="flex justify-between mb-4">
+              <a href="#2023" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">2024</a>
+            </div>
+            <p class="text-gray-700 mb-6"><ol class="list-disc list-inside">
+              <li>Working for <a href="https://stedin.net" target="_blank">Stedin</a></li>
+              <li>Competition Leader (Seniors) & Chess Trainer (Juniors) @ <a href="https://depion.nl" target="_blank">S.V. De
+                  Pion</a></li>
+            <li>Youth Leader @ <a href="https://nbsb.nl" target="_blank">NBSB</a> as of September</li>
+
+            </ol>
+              </p>
+          </div>
+        </div>
+      </li>
   <li class="border-l-2 odd:border-green-600 even:border-purple-600">
     <div class="md:flex flex-start" id="2023">
       <div class="bg-purple-600 w-6 h-6 flex items-center justify-center rounded-full -ml-3.5">
