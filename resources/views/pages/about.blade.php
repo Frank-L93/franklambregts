df1bf11c549d699314994eef29fb56b0f9d59c6b
Frank Lambregts <frank@franklambregts.com>
2024-10-17 11:04:55 +0200

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -24,7 +24,6 @@
                 <flux:accordion.item expanded heading="The first post">
                     <flux:accordion.content>
                         So, we are introducing Flux in the mix of this page. I might start blogging about chess, technology, Harry Potter and other fun stuff.
-                        I am learning a bit of GIT stuff.
                      </flux:accordion.content>
                 </flux:accordion.item>
 
