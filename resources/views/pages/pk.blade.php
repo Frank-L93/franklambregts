f6b14596b4f3049ca71c2865e4aeaa562360b0ec
Frank Lambregts <frank@franklambregts.com>
2024-12-27 13:28:52 +0100

    better layout

--- resources/views/pages/pk.blade.php
+++ resources/views/pages/pk.blade.php
@@ -6,22 +6,22 @@
 <main class="max-w-7xl mx-auto p-6 lg:p-8">
     <div class="text-2xl font-semibold mx-auto text-red-300 text-center">PK Jeugd 2024-2025</div>
 
-    <div>
+    <div class="divide-y-2">
         <h1 class="text-lg">Toernooien</h1>
-        <h2 class="text-md">Klik op de categorie om naar de indeling en uitslagen te gaan</h2>
-    </div>
+        <h2 class="text-md text-gray-400">Klik op de categorie om naar de indeling en uitslagen te gaan</h2>
 
 
 
-        <div class="text-black">
-            <a href="/uploads/2025/AB/SMWSite/index.html"><span class="text-lg">AB</span></a>
 
-            <a href="/uploads/2025/C/SMWSite/index.html"><span class="text-lg">C</span></a>
+        <div class="grid grid-cols-1">
+            <a href="/uploads/2025/AB/SMWSite/index.html" class="text-black hover:text-red-300">AB</a>
 
-            <a href="/uploads/2025/Dm/SMWSite/index.html"><span class="text-lg">D-meisjes</span></a>
+            <a href="/uploads/2025/C/SMWSite/index.html" class="text-black hover:text-red-300">C</a>
+
+            <a href="/uploads/2025/Dm/SMWSite/index.html" class="text-black hover:text-red-300">D-meisjes</a>
 
         </div>
-
+    </div>
 
 
 </main>
