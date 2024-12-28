aef9947898b0b22ba0d2df46a5dd733203fa14e3
Frank Lambregts <frank@franklambregts.com>
2024-12-28 08:24:56 +0100

    Em

--- resources/views/pages/pk.blade.php
+++ resources/views/pages/pk.blade.php
@@ -7,19 +7,13 @@
     <div class="text-2xl font-semibold mx-auto text-red-300 text-center">PK Jeugd 2024-2025</div>
 
     <div class="divide-y-2">
-        <h1 class="text-lg">Toernooien</h1>
+        <h1 class="text-lg">Categorieën</h1>
         <h2 class="text-md text-gray-400">Klik op de categorie om naar de indeling en uitslagen te gaan</h2>
-
-
-
-
         <div class="grid grid-cols-1">
             <a href="/uploads/2025/AB/SMWSite/index.html" class="text-black hover:text-red-300">AB</a>
-
             <a href="/uploads/2025/C/SMWSite/index.html" class="text-black hover:text-red-300">C</a>
-
             <a href="/uploads/2025/Dm/SMWSite/index.html" class="text-black hover:text-red-300">D-meisjes</a>
-
+            <a href="/uploads/2025/Em/SMWSite/index.html" class="text-black hover:text-red-300">E-meisjes</a>
         </div>
     </div>
 
