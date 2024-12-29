df4c77ade0d072932ae175a72c72143af1e9990d
Frank Lambregts <frank@franklambregts.com>
2024-12-29 08:31:42 +0100

    new day

--- resources/views/pages/pk.blade.php
+++ resources/views/pages/pk.blade.php
@@ -12,8 +12,12 @@
         <div class="grid grid-cols-1">
             <a href="/uploads/2025/AB/SMWSite/index.html" class="text-black hover:text-red-300">AB</a>
             <a href="/uploads/2025/C/SMWSite/index.html" class="text-black hover:text-red-300">C</a>
+            <a href="/uploads/2025/Cm/SMWSite/index.html" class="text-black hover:text-red-300">C-meisjes</a>
             <a href="/uploads/2025/Dm/SMWSite/index.html" class="text-black hover:text-red-300">D-meisjes</a>
+            <a href="/uploads/2025/E/SMWSite/index.html" class="text-black hover:text-red-300">E</a>
             <a href="/uploads/2025/Em/SMWSite/index.html" class="text-black hover:text-red-300">E-meisjes</a>
         </div>
+        <h1 class="text-lg">Foto's</h1>
+        <h2 class="text-md text-gray-400"><a href="https://adf-foto.com/jalbum/PK%20Jeugd%2028-12-2024/">Foto's dag 2 (ABC, E-meisjes)</a></h2>
     </div>
 </main>
